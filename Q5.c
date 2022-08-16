5ffe739e77a7a7b49d784f70abaf644e00261114
devanshshekhar00 <75908373+devanshshekhar00@users.noreply.github.com>
2022-08-16 15:54:55 +0530

    Create Q5.c

---
+++ Q5.c
@@ -0,0 +1,27 @@
+int main() {
+  int rows,columns;
+  printf("Enter rows\n");
+  scanf("%d",&rows);
+  printf("Enter columns\n");
+  scanf("%d",&columns);
+  int disp[rows][columns];
+   /*Counter variables for the loop*/
+   int i, j;
+   for(i=0; i<rows; i++) {
+      for(j=0;j<columns;j++) {
+         printf("Enter value for disp[%d][%d]:", i, j);
+         scanf("%d", &disp[i][j]);
+      }
+   }
+   for(i=0; i<rows; i++) {
+      for(j=0;j<columns;j++) {
+         if(i==j ) printf("Left diagnal %d",disp[i][j]);
+      }
+   }
+   
+  
+   
+    
+
+    return 0;
+}
