79936b066b72e7dab9d540472f151e6478695c55
devanshshekhar00 <75908373+devanshshekhar00@users.noreply.github.com>
2022-08-18 14:15:42 +0530

    Update Q4.c

--- Q4.c
+++ Q4.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 
 int main() {
-  int rows,columns;
-  printf("Enter rows\n");
-  scanf("%d",&rows);
-  printf("Enter columns\n");
-  scanf("%d",&columns);
-  int disp[rows][columns];
-   /*Counter variables for the loop*/
-   int i, j;
-   for(i=0; i<rows; i++) {
-      for(j=0;j<columns;j++) {
-         printf("Enter value for disp[%d][%d]:", i, j);
-         scanf("%d", &disp[i][j]);
-      }
+   int sum = 0,rows,columns;
+   printf("Enter rows\n");
+   scanf("%d",&rows);
+   printf("Enter columns\n");
+   scanf("%d",&columns);
+   int k[rows][columns];int a = rows*columns;int b = a-1;
+   int i,j;
+   printf("Initiating input ....\n");
+   for(i = 0; i < rows;i++)
+   {
+       for(j=0;j<columns;j++)
+       {
+           printf("enter element for [%d][%d]\t",i,j);
+           scanf("%d",&k[i][j]);
+       }
    }
-   for(i=0; i<rows; i++) {
-      for(j=0;j<columns;j++) {
-         if(i+j == i*j-1 ) printf("Right diagnal %d",disp[i][j]);
-      }
+   for(i = 0; i < rows;i++)
+   {
+       for(j=0;j<columns;j++)
+       {
+           if(i*j == b ) sum += k[i][j];
+       }
    }
-   
-  
-   
-    
+   printf("Sum is %d",sum);
 
     return 0;
 }
