02ac105c7f8af0cdbe618a1ee9ca297f4a3f3f7c
devanshshekhar00 <75908373+devanshshekhar00@users.noreply.github.com>
2022-08-18 13:20:16 +0530

    Update Q2.c

--- Q2.c
+++ Q2.c
@@ -2,36 +2,37 @@
 #include <stdio.h>
 
 int main() {
-    int  n[3][3],k[3][3],product[3][3];
-   /*Counter variables for the loop*/
-   int i, j;
-   printf("Starting for n\n");
-   for(i=0; i<3; i++) {
-      for(j=0;j<3;j++) {
-         printf("Enter value for disp[%d][%d]:", i, j);
-         scanf("%d", &n[i][j]);
-      }
+   int k[3][3],f[3][3];
+   printf("Initiating input for k\n");
+   int i,j;
+   for( i = 0; i < 3;i++)
+   {
+       for( j = 0; j < 3;j++)
+       {
+           printf("Enter input for [%d][%d]\t",i,j);
+           scanf("%d",&k[i][j]);
+           
+       }
    }
-   printf("Starting for k\n");
-   for(i=0; i<3; i++) {
-      for(j=0;j<3;j++) {
-         printf("Enter value for disp[%d][%d]:", i, j);
-         scanf("%d", &k[i][j]);
-      }
+    printf("Initiating input for f\n");
+    for( i = 0; i < 3;i++)
+   {
+       for( j = 0; j < 3;j++)
+       {
+           printf("Enter input for [%d][%d]\t",i,j);
+           scanf("%d",&f[i][j]);
+           
+       }
    }
-   for(i=0; i<3; i++) {
-      for(j=0;j<3;j++) {
-         product[i][j] = n[i][j] * k[i][j];
-      }
+    for( i = 0; i < 3;i++)
+   {
+       for( j = 0; j < 3;j++)
+       {
+           printf("sum for  [%d][%d] is %d\n",i,j,k[i][j]*f[i][j]);
+           
+           
+       }
    }
-   printf("product of the arrays is");
-   for(i=0; i<3; i++) {
-      for(j=0;j<3;j++) {
-         printf("product for [%d][%d]:", i, j);
-         printf("%d\n",product[i][j]);
-      }
-   }
-   
    
 
     return 0;
